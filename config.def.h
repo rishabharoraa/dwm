6f0e05983ab51caa05e307aa4d9070aa2f9caae1
RishabhAroraa <rishabharora.ns@gmail.com>
2021-05-10 21:06:42 +0530

    minor fixes

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 22;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Fira Code:size=12", "FiraCode Nerd Font Mono:size=20" };
 static const char dmenufont[]       = "Fira Code:size=12";
 static const char col_gray1[]       = "#222222";
