0197e3499631baebd0b57a5f86c0e1aa526e01b5
RishabhAroraa <rishabharora.ns@gmail.com>
2021-06-20 11:24:18 +0530

    changed theme

--- config.def.h
+++ config.def.h
@@ -6,13 +6,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 22;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Fira Code:size=12", "FiraCode Nerd Font Mono:size=20" };
-static const char dmenufont[]       = "Fira Code:size=12";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#002b36";
+static const char col_cyan[]        = "#9932cc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, "#00121D" },
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[] =  {"firefox-nightly"};
 static const char *editor[] = {"atom", NULL};
 
--- dwm.c
+++ dwm.c
@@ -736,7 +736,7 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, tw = 0;
-	int tlpad;
+	//int tlpad;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
@@ -776,10 +776,12 @@ drawbar(Monitor *m)
 			/* make sure name will not overlap on tags even when it is very long */
 			mid = mid >= lrpad / 2 ? mid : (lrpad / 2)+92;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-            tlpad = MAX((m->ww - ((int)TEXTW(m->sel->name) - lrpad)) / 2 - x, lrpad / 2);
-            drw_text(drw, x, 0, w, bh, mid-87, m->sel->name, 0);
+			drw_text(drw,x,0,w,bh, lrpad / 2, m->sel->name, 0);
+           // tlpad = MAX((m->ww - ((int)TEXTW(m->sel->name) - lrpad)) / 2 - x, lrpad / 2);
+           // drw_text(drw, x, 0, w, bh, mid-87, m->sel->name, 0);
 			if (m->sel->isfloating)
-                drw_rect(drw, x + boxs + tlpad - lrpad / 2, boxs, boxw, boxw, m->sel->isfixed, 0);
+				drw_rect(drw, x+ boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+               // drw_rect(drw, x + boxs + tlpad - lrpad / 2, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
